44d7e1ea6adea8a35da1cd14f7bf3f4a45536498
jp4024 <jp4024@columbia.edu>
2020-05-15 21:11:57 -0400

    Finishing touches

--- jp4024-final/app.py
+++ jp4024-final/app.py
@@ -3,6 +3,11 @@
 Created on Tue Apr 21 14:57:17 2020
 
 @author: etill
+
+Name: Janelle Ponnor
+UNI: jp4024
+This file contains different routes to the home page, assignments page,
+and classes page.
 """
 
 #import statements
@@ -16,10 +21,12 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+#Route to assignments page
 @app.route("/1006assignments")
 def test123():
     return render_template("1006assignments.html")
 
+#Route to classes page
 @app.route("/1006classes")
 def columbia():
     return render_template("1006classes.html")
